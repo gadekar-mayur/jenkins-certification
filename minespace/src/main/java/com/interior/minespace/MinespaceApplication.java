68b237065e188c85c0d20ffcf24e07c5dea90a13
megha kundur <meghakundur@gmail.com>
2021-08-20 21:28:55 +0530

    First Jenkins Repo Commit

---
+++ minespace/src/main/java/com/interior/minespace/MinespaceApplication.java
@@ -0,0 +1,13 @@
+package com.interior.minespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinespaceApplication.class, args);
+	}
+
+}
---
+++ minespace/src/test/java/com/interior/minespace/MinespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interior.minespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
